3781dae63810c439bb15d5a38f4e7d0167c15a17
Yatsuto <Yatsuto100@gmail.com>
2025-07-31 07:44:23 -0400

    Test: hardcoded API URL to confirm env issue

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API = 'https://simulated-bank-app.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: API,
