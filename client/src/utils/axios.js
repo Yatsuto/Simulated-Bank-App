aa61bba82a6ffe4fe132b55f0e833e460be7f907
Yatsuto <Yatsuto100@gmail.com>
2025-07-31 07:11:12 -0400

    Final fix: correct API URL for Vite axios config

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = process.env.REACT_APP_API_BASE || 'http://localhost:5000';
+const API = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const axiosInstance = axios.create({
   baseURL: API,
